25ec5c50b3de3c768d0ca6d52474700b346ccf04
Gustavo Augusto <81970233+Guxxtavoww@users.noreply.github.com>
2024-11-19 10:00:00 -0300

    Adding stuff

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 
-import { Providers } from '@/providers';
+import { Providers } from '~/providers';
 
 import '@repo/ui/globals.css';
 
