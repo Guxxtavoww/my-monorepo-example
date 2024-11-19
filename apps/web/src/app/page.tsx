424572486d9b1d8a518e63bc70316eb7ab391378
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-11-19 11:10:31 -0300

    Chore: monorepo works

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,10 +1,33 @@
 import { Button } from '@repo/ui/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+  DialogTitle,
+} from '@repo/ui/components/ui/dialog';
 
 export default function Home() {
   return (
     <main className="min-h-svh p-2 space-y-4">
       <h1>Welcome to the home page!</h1>
-      <Button variant="default">Oi</Button>
+      <Button variant="destructive" className="mr-5">Oi</Button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline">Open dialog</Button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>OI</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>Dialog da porra</DialogDescription>
+          <DialogFooter>
+            <DialogDescription>sdadsaassad</DialogDescription>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </main>
   );
 }
