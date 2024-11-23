a8d728ec00273642b73ca44274d81b79c50f373e
Guxxtavoww <gustavoaugustocar@hotmail.com>
2024-11-23 16:03:15 -0300

    Chore: project works

--- apps/web/src/providers/index.tsx
+++ apps/web/src/providers/index.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { HydrationOverlay } from '@builder.io/react-hydration-overlay'
+
 import { Toaster } from '@repo/ui/components/ui/toaster';
 import { ThemeProvider } from '@repo/ui/providers/theme-provider';
 import { TooltipProvider } from '@repo/ui/providers/tooltip-provider';
@@ -7,13 +9,15 @@ import { TanstackProvider } from '@repo/ui/providers/tanstack-provider';
 
 export function Providers({ children }: WithChildren) {
   return (
-    <ThemeProvider>
-      <TanstackProvider>
-        <TooltipProvider>
-          {children}
-          <Toaster />
-        </TooltipProvider>
-      </TanstackProvider>
-    </ThemeProvider>
+    <HydrationOverlay>
+      <ThemeProvider>
+        <TanstackProvider>
+          <TooltipProvider>
+            {children}
+            <Toaster />
+          </TooltipProvider>
+        </TanstackProvider>
+      </ThemeProvider>
+    </HydrationOverlay>
   );
 }
